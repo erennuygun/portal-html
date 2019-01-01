becf757d5810b49a2903031567fc3b17a82dbb5f
root <root@localhost.localdomain>
2019-01-01 21:16:04 +0300

    first commit

---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\html\controllers\console;
+
+
+/**
+ * Default controller for the `html` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
